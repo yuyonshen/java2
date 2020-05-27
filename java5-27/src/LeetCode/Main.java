e1e1673a97f37f0d6bf7da993d4f5068673bc0ab
yuyonshen <2280629901@qq.com>
2020-05-27 21:36:40 +0800

    力扣练习

---
+++ java5-27/src/LeetCode/Main.java
@@ -0,0 +1,37 @@
+package LeetCode;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        Scanner scanner=new Scanner(System.in);
+//        int l=scanner.nextInt();
+//        int r=scanner.nextInt();
+//        add(l,r);
+//    }
+//    public static int add(int l,int r){
+//        if(l%3==0){
+//            if((r-l)%3==0){
+//                return (l-r)/3*2;
+//            }else{
+//                return (r-l)/3*2+(r-l)%3-1;
+//            }
+//        }else{
+//            int a=l-l%3;
+//            int b=l%3;
+//            if(r%3==0){
+//                return (r-a)/3*2+l%3-1;
+//            }else{
+//                return (r-a)/3*2+l%3-1+(r-a)%3-1;
+//            }
+//        }
+        Scanner scanner=new Scanner(System.in);
+       int l=scanner.nextInt();
+        int r=scanner.nextInt();
+        int a=0;
+        while(a<=l){
+            
+        }
+    }
+
+}
