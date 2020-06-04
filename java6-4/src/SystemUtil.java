61fe727be88dfc779061ed80071c8f475dd1733e
yuyonshen <2280629901@qq.com>
2020-06-04 20:56:19 +0800

    编程练习

---
+++ java6-4/src/SystemUtil.java
@@ -0,0 +1,19 @@
+public class SystemUtil {
+//    public static boolean isAdmin(String userld){
+//        return userld.toLowerCase().equals("admin");
+//
+//    }
+//
+//    public static void main(String[] args) {
+//        System.out.println(isAdmin("Admin"));
+//    }
+//public static void main(String[] args) {
+//   Test2 test=new Test();
+//    test.hello()
+//}
+//}
+//class Test{
+//    public static void hello(){
+//        System.out.println("hello");
+//    }
+}
