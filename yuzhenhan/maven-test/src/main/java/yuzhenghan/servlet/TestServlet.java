0ce883db2c396fbb3469aabff04fcb1ca7240453
yuyonshen <2280629901@qq.com>
2020-06-11 21:32:19 +0800

    Servlet

---
+++ yuzhenhan/maven-test/src/main/java/yuzhenghan/servlet/TestServlet.java
@@ -0,0 +1,23 @@
+package yuzhenghan.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/test")
+public class TestServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html; charset=UTF-8");
+
+        PrintWriter pw=resp.getWriter();
+
+        String i=req.getParameter("i");
+    }
+}
