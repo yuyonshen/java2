37d618bc99042fd4794722628868c66938800403
yuyonshen <2280629901@qq.com>
2020-06-17 21:53:01 +0800

    编程练习

---
+++ java6-17/src/main/java/LeetCode/Test.java
@@ -0,0 +1,18 @@
+package LeetCode;
+
+public class Test {
+//    private int i=1;
+//    int a=2;
+//    static int c=10;
+//
+//    public static void main(String[] args) {
+//        Test t=new Test();
+//        System.out.println(t.i);
+//    }
+public static void main(String[] args) {
+    String foot1=args[1];
+    String foot2=args[2];
+    String foot3=args[3];
+    
+}
+}
